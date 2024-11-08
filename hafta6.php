1e7f511323e9b4adfbd1e71b40abb9bdfce6b4f2
pnarrdgn <pnardogan.1907@gmail.com>
2024-11-08 11:39:47 +0300

    Add files via upload

---
+++ hafta6.php
@@ -0,0 +1,78 @@
+<?php 
+/* $sayi=1;
+while($sayi<10)
+{
+    echo "Sayi: ".$sayi."<br>";
+    $sayi++;
+}
+
+*/
+
+/*$sayi=1;
+echo $sayi;
+$toplam=$sayi;
+while($sayi<10)
+{
+    $sayi++;
+    $toplam+=$sayi;
+    echo "+".$sayi;
+}
+echo "=".$toplam;
+*/
+
+/*$sayi=1;
+do 
+{
+    echo $sayi."-Pınar Doğan "."<br>";
+    $sayi++;
+}
+while ($sayi<=5);
+*/
+
+/*
+$sayi=30;
+$a=1;
+do
+{
+    if($sayi%$a==0)
+    {
+        echo $a."sayısı".$sayi."sayısının bir bölenidir"."<br>";
+    }
+    $a++;
+}
+while($a<=$sayi);
+
+*/
+//foreac sadece dizilerde kullanılır.
+/*
+$gunler=array("pazartesi","salı","çarşamba","perşembe","cuma","cumartsi","pazar");
+foreach($gunler as $gun)
+{
+    echo $gun."<br>";
+}
+*/
+/*
+$harfler=array("a","ı","o","u","e","i","ö","ü");
+echo "Alfabedeki kalın ve ince ünlüler listesi"."<br>";
+foreach($harfler as $harf=>$sira)
+{
+    echo $harf." : ".$sira."<br>";
+}
+*/
+for ($i=0;$i<=10;$i++)
+{
+    echo "<b>Gelişim Üniversitesi</b><u>Web Tasarımı Ve Kodlama Programı </u>"."<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
