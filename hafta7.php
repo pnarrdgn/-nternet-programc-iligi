3021901ef0aeee315c4157e3530fa51aa95bdf29
pnarrdgn <pnardogan.1907@gmail.com>
2024-11-15 11:20:50 +0300

    Add files via upload

---
+++ hafta7.php
@@ -0,0 +1,28 @@
+<?php
+/*for($i=1;$i<=10;$i++)
+{
+    //üç farklı şekilde sayıların karesini alabiliyoruz.
+    //echo $i."sayının karesi:
+    ".$i*$i."<br>";
+    //echo $i."sayısının karesi :".
+    ($i**2)."<br>;
+    //echo $i."sayısının karesi:
+    ".pow($i,2)."<br>";
+}
+
+
+
+*/
+//for döngüsü ile puntoyu püyüten isim yazdırma
+/*
+for ($i=1;$i<=7;$i++)
+{
+    echo "<font size=".$i."<b>PINAR DOĞAN</br>"."<br>"."</font>";
+}
+*/
+
+
+
+
+
+?>
